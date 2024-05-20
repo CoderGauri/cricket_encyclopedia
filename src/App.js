663007cd9d3617bab61ef5ea0fefea56ca7e5d73
Gauri <gauriM304@gmail.com>
2024-05-20 14:29:57 +0530

    teams added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 //import logo from './logo.svg';
 import './App.css';
+import TeamList from './TeamList';
 
 function App() {
   return (
     <div className="App">
-     
+     <TeamList/>
      
     </div>
   );
--- src/iplTeams.js
+++ src/iplTeams.js
@@ -1,12 +1,16 @@
+//import './iplteam.css'
+
 const iplTeams = [
-    { id: 1, name: 'Chennai Super Kings', abbreviation: 'CSK', city: 'Chennai' },
-    { id: 2, name: 'Delhi Capitals', abbreviation: 'DC', city: 'Delhi' },
-    { id: 3, name: 'Kings XI Punjab', abbreviation: 'KXIP', city: 'Mohali' },
-    { id: 4, name: 'Kolkata Knight Riders', abbreviation: 'KKR', city: 'Kolkata' },
-    { id: 5, name: 'Mumbai Indians', abbreviation: 'MI', city: 'Mumbai' },
-    { id: 6, name: 'Rajasthan Royals', abbreviation: 'RR', city: 'Jaipur' },
-    { id: 7, name: 'Royal Challengers Bangalore', abbreviation: 'RCB', city: 'Bangalore' },
-    { id: 8, name: 'Sunrisers Hyderabad', abbreviation: 'SRH', city: 'Hyderabad' }
+    { id: 1, name: 'Chennai Super Kings', abbreviation: 'CSK', city: 'Chennai' , captain : "Ruturaj Gaikwad"},
+    { id: 2, name: 'Delhi Capitals', abbreviation: 'DC', city: 'Delhi' , captain : "Rishab Pant" },
+    { id: 3, name: 'Kings XI Punjab', abbreviation: 'KXIP', city: 'Mohali' , captain : "Shikhar Dhawan"},
+    { id: 4, name: 'Kolkata Knight Riders', abbreviation: 'KKR', city: 'Kolkata',captain : "Shreyas Iyer"},
+    { id: 5, name: 'Mumbai Indians', abbreviation: 'MI', city: 'Mumbai',captain :"Hardik Pandya" },
+    { id: 6, name: 'Rajasthan Royals', abbreviation: 'RR', city: 'Jaipur',captain :"Sanju Samson" },
+    { id: 7, name: 'Royal Challengers Bangalore', abbreviation: 'RCB', city: 'Bangalore',captain : "Fap Duplessi"},
+    { id: 8, name: 'Sunrisers Hyderabad', abbreviation: 'SRH', city: 'Hyderabad' , captain :"Pat cummins"},
+    { id: 9, name: 'Gujarat Titans', abbreviation: 'GT', city: 'Gujarat' ,captain : "Shubman Gill"},
+    { id: 10, name: 'Lucknow Super Gaints', abbreviation: 'LSG', city: 'Lucknow',captain : "KL Rahul"}
   ];
   
   export default iplTeams;
